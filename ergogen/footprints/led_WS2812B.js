21fb04f8486d58cf1494c7d98e59b9317e606f14
Vacant0mens <evan.carman@gmail.com>
2024-08-31 22:58:46 -0600

    made the led_WS2812B footprint actually reversible with the pads on the correct sides and the silkscreens facing the right way

--- ergogen/footprints/led_WS2812B.js
+++ ergogen/footprints/led_WS2812B.js
@@ -16,63 +16,67 @@ module.exports = {
             ${p.at /* parametric position */}
 
             ${'' /* footprint reference */}
-            (fp_text reference "${p.ref}" (at 0 0 ${90 + p.r}) (layer ${p.side}.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
-            (fp_text value "" (at 0 0) (layer ${p.side}.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
+            (fp_text reference "${p.ref}" (at 0 0 ${90 + p.r}) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
+            (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
+            `
+        const front_content = `
+            (fp_line (start -2.5 -2.5) (end -2.5 2.5) (layer F.SilkS) (width 0.15))
+            (fp_line (start -2.5 2.5) (end 2.5 2.5) (layer F.SilkS) (width 0.15))
+            (fp_line (start 2.5 2.5) (end 2.5 -2.5) (layer F.SilkS) (width 0.15))
+            (fp_line (start 2.5 -2.5) (end -2.5 -2.5) (layer F.SilkS) (width 0.15))
 
-            (fp_line (start -2.5 -2.5) (end -2.5 2.5) (layer ${p.side}.SilkS) (width 0.15))
-            (fp_line (start -2.5 2.5) (end 2.5 2.5) (layer ${p.side}.SilkS) (width 0.15))
-            (fp_line (start 2.5 2.5) (end 2.5 -2.5) (layer ${p.side}.SilkS) (width 0.15))
-            (fp_line (start 2.5 -2.5) (end -2.5 -2.5) (layer ${p.side}.SilkS) (width 0.15))
+            (fp_line (start -3.5 -3) (end -3.5 3) (layer F.SilkS) (width 0.15))
+            (fp_line (start -3.5 3) (end 3.5 3) (layer F.SilkS) (width 0.15))
+            (fp_line (start 3.5 3) (end 3.5 -3) (layer F.SilkS) (width 0.15))
+            (fp_line (start 3.5 -3) (end -3.5 -3) (layer F.SilkS) (width 0.15))
 
-            (fp_line (start -3.5 -3) (end -3.5 3) (layer ${p.side}.SilkS) (width 0.15))
-            (fp_line (start -3.5 3) (end 3.5 3) (layer ${p.side}.SilkS) (width 0.15))
-            (fp_line (start 3.5 3) (end 3.5 -3) (layer ${p.side}.SilkS) (width 0.15))
-            (fp_line (start 3.5 -3) (end -3.5 -3) (layer ${p.side}.SilkS) (width 0.15))
-
-            (fp_poly (pts (xy 3.5 3) (xy 3.5 2) (xy 2 3)) (layer ${p.side}.SilkS) (width 0.1))
+            (fp_poly (pts (xy 3.5 2) (xy 3.5 3) (xy 2.5 3)) (layer F.SilkS) (width 0.1))
             
-            (pad 1 smd rect (at -2.2 -0.875 ${p.r}) (size 2.6 1) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.VCC})
-            (pad 2 smd rect (at -2.2 0.875 ${p.r}) (size 2.6 1) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.dout})
-            (pad 3 smd rect (at 2.2 0.875 ${p.r}) (size 2.6 1) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.GND})
-            (pad 4 smd rect (at 2.2 -0.875 ${p.r}) (size 2.6 1) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.din})
+            (pad 1 smd rect (at -2.2 -0.875 ${p.r}) (size 2.6 1) (layers F.Cu F.Paste F.Mask) ${p.VCC})
+            (pad 2 smd rect (at -2.2 0.875 ${p.r}) (size 2.6 1) (layers F.Cu F.Paste F.Mask) ${p.dout})
+            (pad 3 smd rect (at 2.2 0.875 ${p.r}) (size 2.6 1) (layers F.Cu F.Paste F.Mask) ${p.GND})
+            (pad 4 smd rect (at 2.2 -0.875 ${p.r}) (size 2.6 1) (layers F.Cu F.Paste F.Mask) ${p.din})
 
-            (pad 11 smd rect (at -2.5 -1.6 ${p.r}) (size 2 1.2) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.VCC})
-            (pad 22 smd rect (at -2.5 1.6 ${p.r}) (size 2 1.2) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.dout})
-            (pad 33 smd rect (at 2.5 1.6 ${p.r}) (size 2 1.2) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.GND})
-            (pad 44 smd rect (at 2.5 -1.6 ${p.r}) (size 2 1.2) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.din})
+            (pad 11 smd rect (at -2.5 -1.6 ${p.r}) (size 2 1.2) (layers F.Cu F.Paste F.Mask) ${p.VCC})
+            (pad 22 smd rect (at -2.5 1.6 ${p.r}) (size 2 1.2) (layers F.Cu F.Paste F.Mask) ${p.dout})
+            (pad 33 smd rect (at 2.5 1.6 ${p.r}) (size 2 1.2) (layers F.Cu F.Paste F.Mask) ${p.GND})
+            (pad 44 smd rect (at 2.5 -1.6 ${p.r}) (size 2 1.2) (layers F.Cu F.Paste F.Mask) ${p.din})
         `
         const common_end = `
         )
         `
-        const reverse_content = `
-            (fp_text value "%R" (at 0 1.2 ${90 + p.r}) (layer ${reverse_side}.SilkS) (effects (font (size 1 1) (thickness 0.15)) (justify mirror)))
-            (fp_line (start -2.5 -2.5) (end -2.5 2.5) (layer ${reverse_side}.SilkS) (width 0.15))
-            (fp_line (start -2.5 2.5) (end 2.5 2.5) (layer ${reverse_side}.SilkS) (width 0.15))
-            (fp_line (start 2.5 2.5) (end 2.5 -2.5) (layer ${reverse_side}.SilkS) (width 0.15))
-            (fp_line (start 2.5 -2.5) (end -2.5 -2.5) (layer ${reverse_side}.SilkS) (width 0.15))
+        const back_content = `
+            (fp_text value "%R" (at 0 0 ${90 + p.r}) (layer B.SilkS) (effects (font (size 1 1) (thickness 0.15)) (justify mirror)))
+            (fp_line (start -2.5 -2.5) (end -2.5 2.5) (layer B.SilkS) (width 0.15))
+            (fp_line (start -2.5 2.5) (end 2.5 2.5) (layer B.SilkS) (width 0.15))
+            (fp_line (start 2.5 2.5) (end 2.5 -2.5) (layer B.SilkS) (width 0.15))
+            (fp_line (start 2.5 -2.5) (end -2.5 -2.5) (layer B.SilkS) (width 0.15))
 
-            (fp_line (start -3.5 -3) (end -3.5 3) (layer ${reverse_side}.SilkS) (width 0.15))
-            (fp_line (start -3.5 3) (end 3.5 3) (layer ${reverse_side}.SilkS) (width 0.15))
-            (fp_line (start 3.5 3) (end 3.5 -3) (layer ${reverse_side}.SilkS) (width 0.15))
-            (fp_line (start 3.5 -3) (end -3.5 -3) (layer ${reverse_side}.SilkS) (width 0.15))
+            (fp_line (start -3.5 -3) (end -3.5 3) (layer B.SilkS) (width 0.15))
+            (fp_line (start -3.5 3) (end 3.5 3) (layer B.SilkS) (width 0.15))
+            (fp_line (start 3.5 3) (end 3.5 -3) (layer B.SilkS) (width 0.15))
+            (fp_line (start 3.5 -3) (end -3.5 -3) (layer B.SilkS) (width 0.15))
 
-            (fp_poly (pts (xy 3.5 3) (xy 3.5 2) (xy 2 3)) (layer ${reverse_side}.SilkS) (width 0.1))
+            (fp_poly (pts (xy 3.5 -3) (xy 3.5 -2) (xy 2.5 -3)) (layer B.SilkS) (width 0.1))
 
-            (pad 1 smd rect (at -2.2 -0.875 ${p.r}) (size 2.6 1) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.VCC})
-            (pad 2 smd rect (at -2.2 0.875 ${p.r}) (size 2.6 1) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.dout})
-            (pad 3 smd rect (at 2.2 0.875 ${p.r}) (size 2.6 1) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.GND})
-            (pad 4 smd rect (at 2.2 -0.875 ${p.r}) (size 2.6 1) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.din})
+            (pad 1 smd rect (at -2.2 -0.875 ${p.r}) (size 2.6 1) (layers B.Cu B.Paste B.Mask) ${p.dout})
+            (pad 2 smd rect (at -2.2 0.875 ${p.r}) (size 2.6 1) (layers B.Cu B.Paste B.Mask) ${p.VCC})
+            (pad 3 smd rect (at 2.2 0.875 ${p.r}) (size 2.6 1) (layers B.Cu B.Paste B.Mask) ${p.din})
+            (pad 4 smd rect (at 2.2 -0.875 ${p.r}) (size 2.6 1) (layers B.Cu B.Paste B.Mask) ${p.GND})
 
-            (pad 11 smd rect (at -2.5 -1.6 ${p.r}) (size 2 1.2) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.VCC})
-            (pad 22 smd rect (at -2.5 1.6 ${p.r}) (size 2 1.2) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.dout})
-            (pad 33 smd rect (at 2.5 1.6 ${p.r}) (size 2 1.2) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.GND})
-            (pad 44 smd rect (at 2.5 -1.6 ${p.r}) (size 2 1.2) (layers ${p.side}.Cu ${p.side}.Paste ${p.side}.Mask) ${p.din})
+            (pad 11 smd rect (at -2.5 -1.6 ${p.r}) (size 2 1.2) (layers B.Cu B.Paste B.Mask) ${p.dout})
+            (pad 22 smd rect (at -2.5 1.6 ${p.r}) (size 2 1.2) (layers B.Cu B.Paste B.Mask) ${p.VCC})
+            (pad 33 smd rect (at 2.5 1.6 ${p.r}) (size 2 1.2) (layers B.Cu B.Paste B.Mask) ${p.din})
+            (pad 44 smd rect (at 2.5 -1.6 ${p.r}) (size 2 1.2) (layers B.Cu B.Paste B.Mask) ${p.GND})
         `
 
         let final = common_start;
+        if (p.reversible || p.side == 'F') {
+            final += front_content
+        }
 
-        if (p.reversible) {
-            final += reverse_content
+        if (p.reversible || p.side == 'B') {
+            final += back_content
         }
 
         final += common_end;
